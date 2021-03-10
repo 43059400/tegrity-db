7746978b0c4ad941bfbb8b775c0b9e74aa5deb19
valt <pmaxcypb@gmail.com>
2021-03-10 00:13:04 -0500

    cors

--- App.js
+++ App.js
@@ -23,7 +23,11 @@ let interval
 
 app.use(cookieParser())
 app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('You have activated Skynet!')
